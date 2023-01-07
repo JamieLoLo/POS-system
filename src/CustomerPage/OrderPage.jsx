bfb897707b2bc00348b6b55fc9d23621e7f5eb22
JamieLoLo <jamielo0320@gmail.com>
2023-01-07 21:03:28 +0800

    Test

--- src/CustomerPage/OrderPage.jsx
+++ src/CustomerPage/OrderPage.jsx
@@ -12,11 +12,6 @@ import { ReactComponent as CartIcon } from '../CustomerComponents/assets/icon/ca
 import styles from './OrderPage.module.scss'
 
 const OrderPage = () => {
-  window.addEventListener('resize', () => {
-    let vh = window.innerHeight * 0.01
-    document.documentElement.style.setProperty('--vh', `${vh}px`)
-  })
-
   return (
     <div className='mobile__main__container'>
       <header>
@@ -51,7 +46,7 @@ const OrderPage = () => {
           <CustomerMenuItem
             dish='無錫排骨飯'
             description='我是一個描述，描述描述描述啊啊啊。我是一個描述，描述描述描述啊啊啊。我是一個描述，描述描述描述啊啊啊。'
-            price='$120'
+            price='$420'
             count='1'
           />
           <CustomerMenuItem dish='奶油明太子義大利麵' price='$320' count='1' />
