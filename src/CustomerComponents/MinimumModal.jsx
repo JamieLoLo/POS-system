a8d1460b15cc613211abbe0cafc4d7b6cea6dc64
JamieLoLo <jamielo0320@gmail.com>
2023-01-07 21:24:47 +0800

    Fix safari 100vh

--- src/CustomerComponents/MinimumModal.jsx
+++ src/CustomerComponents/MinimumModal.jsx
@@ -5,22 +5,20 @@ import styles from './MinimumModal.module.scss'
 
 const MinimumModal = () => {
   return (
-    <div className='mobile__main__container'>
-      <div className={styles.modal}>
-        <div className={styles.backdrop}></div>
-        <div className={styles.modal__container}>
-          <div className={styles.text__container}>
-            <div className={styles.title}>未達低消</div>
-            <div className={styles.subtitle}>
-              提醒您：距離最低消費金額還差
-              <span className={styles.difference}> $100 </span>元
-            </div>
+    <div className={styles.modal}>
+      <div className={styles.backdrop}></div>
+      <div className={styles.modal__container}>
+        <div className={styles.text__container}>
+          <div className={styles.title}>未達低消</div>
+          <div className={styles.subtitle}>
+            提醒您：距離最低消費金額還差
+            <span className={styles.difference}> $100 </span>元
           </div>
-          <div className={styles.button__container}>
-            <Link to='/customer/main'>
-              <button className={styles.order__button}>繼續點餐</button>
-            </Link>
-          </div>
+        </div>
+        <div className={styles.button__container}>
+          <Link to='/customer/main'>
+            <button className={styles.order__button}>繼續點餐</button>
+          </Link>
         </div>
       </div>
     </div>
