9e9e37204501864d63e6bd98a81be9f3f89fd2a6
JamieLoLo <jamielo0320@gmail.com>
2023-01-09 03:00:15 +0800

    Add react-datepicker

--- src/POSPage/MonthlyRevenuePage.jsx
+++ src/POSPage/MonthlyRevenuePage.jsx
@@ -1,28 +1,42 @@
 import React from 'react'
-import Select from 'react-select'
 // hook
 import { useLocation } from 'react-router-dom'
+import { useState } from 'react'
 // UI
 import { PosMainGridSystem } from '../POSLayout/GridSystem'
 import { FormSwitchButton, RevenueItem } from '../POSComponents/index'
-// SCSS
+// calendar package
+import DatePicker from 'react-datepicker'
+// CSS
+import 'react-datepicker/dist/react-datepicker.css'
 import styles from './MonthlyRevenuePage.module.scss'
 
 const MonthlyRevenuePage = () => {
   const pathname = useLocation().pathname
-  const options = [
-    { value: '2022/12', label: '2022/12' },
-    { value: '2023/1', label: '2023/1' },
-    { value: '2023/2', label: '2023/2' },
-  ]
+  const [startDate, setStartDate] = useState(new Date())
+  const [endDate, setEndDate] = useState(new Date())
+
   return (
-    <div>
+    <div className='main__container'>
       <PosMainGridSystem pathname={pathname}>
         <div className={styles.container__height}>
           <button className={styles.export__button}>匯出 Excel</button>
           <FormSwitchButton page='revenue' />
           <div className={styles.input__container}>
-            <Select className={styles.select} options={options} />
+            <DatePicker
+              className={styles.inner__container}
+              wrapperClassName={styles.datePicker}
+              selected={startDate}
+              onChange={(date) => setStartDate(date)}
+            />
+            至
+            <DatePicker
+              className={styles.inner__container}
+              wrapperClassName={styles.datePicker}
+              selected={endDate}
+              onChange={(date) => setEndDate(date)}
+            />
+            <button className={styles.search}>查詢</button>
           </div>
           <div className={styles.list__container}>
             <div className={styles.title__container}>
@@ -32,6 +46,33 @@ const MonthlyRevenuePage = () => {
               <div className={styles.title}>客單價</div>
             </div>
             <div className={styles.classification__list}>
+              <RevenueItem date='12/01' revenue='$12,000' tc='50' aov='$251' />
+              <RevenueItem date='12/01' revenue='$12,000' tc='50' aov='$251' />
+              <RevenueItem date='12/01' revenue='$12,000' tc='50' aov='$251' />
+              <RevenueItem
+                date='12/01'
+                revenue='$12,000'
+                tc='50'
+                aov='$251'
+              />{' '}
+              <RevenueItem date='12/01' revenue='$12,000' tc='50' aov='$251' />
+              <RevenueItem date='12/01' revenue='$12,000' tc='50' aov='$251' />
+              <RevenueItem date='12/01' revenue='$12,000' tc='50' aov='$251' />
+              <RevenueItem
+                date='12/01'
+                revenue='$12,000'
+                tc='50'
+                aov='$251'
+              />{' '}
+              <RevenueItem date='12/01' revenue='$12,000' tc='50' aov='$251' />
+              <RevenueItem date='12/01' revenue='$12,000' tc='50' aov='$251' />
+              <RevenueItem date='12/01' revenue='$12,000' tc='50' aov='$251' />
+              <RevenueItem
+                date='12/01'
+                revenue='$12,000'
+                tc='50'
+                aov='$251'
+              />{' '}
               <RevenueItem date='12/01' revenue='$12,000' tc='50' aov='$251' />
               <RevenueItem date='12/01' revenue='$12,000' tc='50' aov='$251' />
               <RevenueItem date='12/01' revenue='$12,000' tc='50' aov='$251' />
--- src/POSPage/RankPage.jsx
+++ src/POSPage/RankPage.jsx
@@ -1,28 +1,41 @@
 import React from 'react'
-import Select from 'react-select'
 // hook
 import { useLocation } from 'react-router-dom'
+import { useState } from 'react'
 // UI
 import { PosMainGridSystem } from '../POSLayout/GridSystem'
 import { FormSwitchButton, RankItem } from '../POSComponents/index'
-// SCSS
+// calendar package
+import DatePicker from 'react-datepicker'
+// CSS
+import 'react-datepicker/dist/react-datepicker.css'
 import styles from './RankPage.module.scss'
 
 const RankPage = () => {
   const pathname = useLocation().pathname
-  const options = [
-    { value: '2022/12/1', label: '2022/12/1' },
-    { value: '2022/12/2', label: '2022/12/2' },
-    { value: '2022/12/3', label: '2022/12/3' },
-  ]
+  const [startDate, setStartDate] = useState(new Date())
+  const [endDate, setEndDate] = useState(new Date())
   return (
-    <div>
+    <div className='main__container'>
       <PosMainGridSystem pathname={pathname}>
         <div className={styles.container__height}>
           <button className={styles.export__button}>匯出 Excel</button>
           <FormSwitchButton page='rank' />
           <div className={styles.input__container}>
-            <Select className={styles.select} options={options} />
+            <DatePicker
+              className={styles.inner__container}
+              wrapperClassName={styles.datePicker}
+              selected={startDate}
+              onChange={(date) => setStartDate(date)}
+            />
+            至
+            <DatePicker
+              className={styles.inner__container}
+              wrapperClassName={styles.datePicker}
+              selected={endDate}
+              onChange={(date) => setEndDate(date)}
+            />
+            <button className={styles.search}>查詢</button>
           </div>
           <div className={styles.list__container}>
             <div className={styles.title__container}>
@@ -30,7 +43,22 @@ const RankPage = () => {
               <div className={styles.title}>數量</div>
               <div className={styles.title}>小計</div>
             </div>
-            <div className={styles.classification__list}>
+            <div className={styles.rank__list}>
+              <RankItem dish='紅燒牛腩筋飯' counts='10' total='$3200' />
+              <RankItem dish='無錫排骨飯' counts='5' total='$1200' />
+              <RankItem dish='蜜椒小豬球' counts='5' total='$3400' />
+              <RankItem dish='紅燒牛腩筋飯' counts='10' total='$3200' />
+              <RankItem dish='無錫排骨飯' counts='5' total='$1200' />
+              <RankItem dish='蜜椒小豬球' counts='5' total='$3400' />{' '}
+              <RankItem dish='紅燒牛腩筋飯' counts='10' total='$3200' />
+              <RankItem dish='無錫排骨飯' counts='5' total='$1200' />
+              <RankItem dish='蜜椒小豬球' counts='5' total='$3400' />{' '}
+              <RankItem dish='紅燒牛腩筋飯' counts='10' total='$3200' />
+              <RankItem dish='無錫排骨飯' counts='5' total='$1200' />
+              <RankItem dish='蜜椒小豬球' counts='5' total='$3400' />{' '}
+              <RankItem dish='紅燒牛腩筋飯' counts='10' total='$3200' />
+              <RankItem dish='無錫排骨飯' counts='5' total='$1200' />
+              <RankItem dish='蜜椒小豬球' counts='5' total='$3400' />{' '}
               <RankItem dish='紅燒牛腩筋飯' counts='10' total='$3200' />
               <RankItem dish='無錫排骨飯' counts='5' total='$1200' />
               <RankItem dish='蜜椒小豬球' counts='5' total='$3400' />
