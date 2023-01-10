011611e87f71ece36389baf4fb10711484f2380c
JamieLoLo <jamielo0320@gmail.com>
2023-01-10 18:44:12 +0800

    Add update manage store

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 import modalSlice from './modal-slice'
+import updateSlice from './update-slice'
 
 const store = configureStore({
   reducer: {
     modal: modalSlice,
+    update: updateSlice,
   },
 })
 
