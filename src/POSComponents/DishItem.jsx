0e21d29d8bd3ca999ab98ba544613f942d96d1d8
JamieLoLo <jamielo0320@gmail.com>
2023-01-12 01:32:40 +0800

    Remove delete button

--- src/POSComponents/DishItem.jsx
+++ src/POSComponents/DishItem.jsx
@@ -1,15 +1,29 @@
 import React from 'react'
+// hook
+import { useDispatch, useSelector } from 'react-redux'
+// store
+import { modalActions } from '../store/modal-slice'
+import { informationActions } from '../store/information-slice'
 // SCSS
 import styles from './DishItem.module.scss'
 
-const DishItem = ({ dish, classification }) => {
+const DishItem = ({ data }) => {
+  const dispatch = useDispatch()
+
+  // useSelector
+  const modifyHandler = () => {
+    dispatch(informationActions.setProductInfo(data))
+    dispatch(modalActions.setIsModifyProductModalOpen(true))
+  }
+
   return (
     <div className={styles.list__container}>
-      <div className={styles.list__item}>{dish}</div>
-      <div className={styles.list__item}>{classification}</div>
+      <div className={styles.list__item}>{data.name}</div>
+      <div className={styles.list__item}>{data.Category.name}</div>
       <div className={styles.button__container}>
-        <button className={styles.modify__button}>修改</button>
-        <button className={styles.delete__button}>刪除</button>
+        <button className={styles.modify__button} onClick={modifyHandler}>
+          修改
+        </button>
       </div>
     </div>
   )
