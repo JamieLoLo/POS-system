9df40dd5d22224c1d379f19045fd63edf043e269
JamieLoLo <jamielo0320@gmail.com>
2023-01-13 01:56:27 +0800

    Add scrollIntoView

--- src/CustomerComponents/CustomerOrderCategory.jsx
+++ src/CustomerComponents/CustomerOrderCategory.jsx
@@ -1,20 +1,30 @@
 import clsx from 'clsx'
 import React from 'react'
 // hook
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 // SCSS
 import styles from './CustomerOrderCategory.module.scss'
 
 const CustomerOrderCategory = ({ data, onClick }) => {
-  const targetHandler = (e) => {
-    localStorage.setItem('active_category', e.target.innerText)
-  }
+  // localStorage
   const activeCategory = localStorage.getItem('active_category')
   const defaultCategory = localStorage.getItem('default_category_name')
 
+  // 載入頁面當下取得第一個分類的id，用來渲染初始畫面。
   useEffect(() => {
     localStorage.setItem('active_category', defaultCategory)
-  }, [])
+  }, [defaultCategory])
+
+  // 獲取 DOM，用來進行scrollIntoView。
+  const scrollRef = useRef(undefined)
+
+  const targetHandler = (e) => {
+    localStorage.setItem('active_category', e.target.innerText)
+    scrollRef.current.scrollIntoView({
+      inline: 'center',
+      behavior: 'smooth',
+    })
+  }
 
   return (
     <div
@@ -26,6 +36,7 @@ const CustomerOrderCategory = ({ data, onClick }) => {
         onClick?.(data.id)
         targetHandler(e)
       }}
+      ref={scrollRef}
     >
       {data.name}
     </div>
--- src/POSComponents/OrderCategory.jsx
+++ src/POSComponents/OrderCategory.jsx
@@ -1,12 +1,19 @@
 import clsx from 'clsx'
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 
 // SCSS
 import styles from './OrderCategory.module.scss'
 
 const OrderCategory = ({ data, onClick }) => {
+  // 獲取 DOM，用來進行scrollIntoView。
+  const scrollRef = useRef(undefined)
+
   const targetHandler = (e) => {
     localStorage.setItem('active_category', e.target.innerText)
+    scrollRef.current.scrollIntoView({
+      inline: 'center',
+      behavior: 'smooth',
+    })
   }
   const activeCategory = localStorage.getItem('active_category')
   const defaultCategory = localStorage.getItem('default_category_name')
@@ -25,6 +32,7 @@ const OrderCategory = ({ data, onClick }) => {
         onClick?.(data.id)
         targetHandler(e)
       }}
+      ref={scrollRef}
     >
       {data.name}
     </div>
