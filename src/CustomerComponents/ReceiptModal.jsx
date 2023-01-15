c4f6514c37c09fa0388ec3bfee564999a5aff56b
JamieLoLo <jamielo0320@gmail.com>
2023-01-15 18:06:41 +0800

    Add localStorage data

--- src/CustomerComponents/ReceiptModal.jsx
+++ src/CustomerComponents/ReceiptModal.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+// hook
+import { useSelector } from 'react-redux'
 // SCSS
 import styles from './ReceiptModal.module.scss'
 
 const ReceiptModal = () => {
-  return (
+  // localStorage
+  const orderId = localStorage.getItem('order_id')
+  const adultCount = localStorage.getItem('adult_count')
+  const childrenCount = localStorage.getItem('children_count')
+  const totalPrice = localStorage.getItem('total_price')
+  const tableName = localStorage.getItem('table_name')
+  // useSelector
+  const isReceiptModalOpen = useSelector(
+    (state) => state.modal.isReceiptModalOpen
+  )
+
+  return isReceiptModalOpen ? (
     <div className={styles.modal}>
       <div className={styles.backdrop}></div>
       <div className={styles.modal__container}>
@@ -18,23 +30,27 @@ const ReceiptModal = () => {
         <div className={styles.detail}>
           <div className={styles.item__container}>
             <div className={styles.detail__title}>單號</div>
-            <div className={styles.detail__number}>015</div>
+            <div className={styles.detail__number}>{orderId}</div>
           </div>
           <div className={styles.item__container}>
             <div className={styles.detail__title}>桌號</div>
-            <div className={styles.detail__number}>22</div>
+            <div className={styles.detail__number}>{tableName}</div>
           </div>
           <div className={styles.item__container}>
             <div className={styles.detail__title}>人數</div>
-            <div className={styles.detail__number}>1大1小</div>
+            <div className={styles.detail__number}>
+              {adultCount}大{childrenCount}小
+            </div>
           </div>
           <div className={styles.item__container}>
             <div className={styles.detail__title}>金額</div>
-            <div className={styles.detail__number}>$320</div>
+            <div className={styles.detail__number}>${totalPrice}</div>
           </div>
         </div>
       </div>
     </div>
+  ) : (
+    ''
   )
 }
 
