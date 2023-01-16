cda9f44b94788c36e1fde8484c0e79324a4ae255
JamieLoLo <jamielo0320@gmail.com>
2023-01-16 22:00:02 +0800

    Connect finish order api

--- src/POSComponents/OrderTableItem.jsx
+++ src/POSComponents/OrderTableItem.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import clsx from 'clsx'
+import Swal from 'sweetalert2'
 // hook
 import { useNavigate } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import { useEffect } from 'react'
 // store
 import { informationActions } from '../store/information-slice'
+import { updateActions } from '../store/update-slice'
 // api
 import { getOrderApi } from '../api/orderApi'
-import { getMinimumApi } from '../api/posApi'
+import { getMinimumApi, finishOrderApi } from '../api/posApi'
 // SCSS
 import styles from './OrderTableItem.module.scss'
 
@@ -31,6 +33,7 @@ const OrderTableItem = ({ data }) => {
 
   // 點擊桌子後將桌子資訊存入store，判斷目前狀態，導入對應頁面。
   const orderHandler = async () => {
+    console.log(data)
     if (data.name === '0') {
       return
     }
@@ -58,6 +61,45 @@ const OrderTableItem = ({ data }) => {
         localStorage.setItem('cart_list', JSON.stringify(cartList))
         localStorage.setItem('render_cart_list', JSON.stringify(renderCartList))
         navigate('/order/system')
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    // 客人離場
+    if (data.Orders.isPaid === 1) {
+      try {
+        let result = await Swal.fire({
+          title: '客人離場了嗎？',
+          text: '確定後將無法恢復！',
+          icon: 'warning',
+          showCancelButton: true,
+          cancelButtonText: '取消',
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: '確定',
+        })
+        if (result.isConfirmed) {
+          const res = await finishOrderApi(data.Orders.id)
+          if (res.status !== 200) {
+            Swal.fire({
+              position: 'center',
+              icon: 'error',
+              title: '發生錯誤，請重新操作。',
+              showConfirmButton: false,
+              timer: 2000,
+            })
+            return
+          }
+          dispatch(updateActions.setIsTableUpdate())
+          Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: '設定成功',
+            showConfirmButton: false,
+            timer: 2000,
+          })
+          console.log(res)
+        }
       } catch (error) {
         console.error(error)
       }
