471f032a8a2129c1e785ec13f87a1bf7bc373d19
JamieLoLo <jamielo0320@gmail.com>
2023-01-16 19:10:51 +0800

    Add onClick function

--- src/POSComponents/MenuItem.jsx
+++ src/POSComponents/MenuItem.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 // SCSS
 import styles from './MenuItem.module.scss'
 
-const MenuItem = ({ data }) => {
+const MenuItem = ({ data, addMenuItemHandler }) => {
   return (
-    <div className={styles.menu__item}>
+    <div
+      className={styles.menu__item}
+      onClick={() => {
+        addMenuItemHandler?.(data.id, data.price, data.name)
+      }}
+    >
       <div className={styles.title}>{data.name}</div>
       <div className={styles.price}>${data.price}</div>
     </div>
