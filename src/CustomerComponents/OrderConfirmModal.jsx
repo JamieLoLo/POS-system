72ae4569b15055fa698405e1fde0a8a38052f033
JamieLoLo <jamielo0320@gmail.com>
2023-01-17 22:12:58 +0800

    Delete unnecessary data

--- src/CustomerComponents/OrderConfirmModal.jsx
+++ src/CustomerComponents/OrderConfirmModal.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import Swal from 'sweetalert2'
 // hook
 import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 // store
 import { modalActions } from '../store/modal-slice'
 // api
@@ -10,11 +12,10 @@ import styles from './OrderConfirmModal.module.scss'
 
 const OrderConfirmModal = () => {
   const dispatch = useDispatch()
+  const navigate = useNavigate()
   // localStorage
-  const cartList = JSON.parse(localStorage.getItem('cart_list'))
   const checkoutList = JSON.parse(localStorage.getItem('checkout_list'))
   const orderId = Number(localStorage.getItem('order_id'))
-  const tableId = localStorage.getItem('table_id')
   const minCharge = localStorage.getItem('min_charge')
   const adultNum = localStorage.getItem('adult_count')
   const totalPrice = localStorage.getItem('total_price')
@@ -39,7 +40,16 @@ const OrderConfirmModal = () => {
       }
       const res = await customerOrderApi(orderId, checkoutList)
       if (res.status === 200) {
-        dispatch(modalActions.setIsReceiptModalOpen(true))
+        navigate('/customer/receipt')
+      }
+      if (res.status !== 200) {
+        Swal.fire({
+          position: 'center',
+          icon: 'error',
+          title: '點餐失敗，請洽櫃檯。',
+          showConfirmButton: false,
+          timer: 2000,
+        })
       }
     } catch (error) {
       console.error(error)
@@ -54,11 +64,11 @@ const OrderConfirmModal = () => {
           <div className={styles.title}>要送出訂單了嗎？</div>
         </div>
         <div className={styles.button__container}>
-          <button className={styles.return__button} onClick={returnHandler}>
-            返回
-          </button>
           <button className={styles.confirm__button} onClick={confirmHandler}>
             確定
+          </button>
+          <button className={styles.return__button} onClick={returnHandler}>
+            返回
           </button>
         </div>
       </div>
