75ac999bc8f43624b526290c6de14f6288dc130e
JamieLoLo <jamielo0320@gmail.com>
2023-01-18 03:06:35 +0800

    Add calculate function

--- src/POSPage/AccountClosingPage.jsx
+++ src/POSPage/AccountClosingPage.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 // hook
 import { useLocation, useNavigate } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
-import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect, useState } from 'react'
 // UI
 import { PosMainGridSystem } from '../POSLayout/GridSystem'
 import { FormSwitchButton, AccountClosingModal } from '../POSComponents/index'
+// api
+import { getUnsettledRevenueApi } from '../api/posApi'
 // store
 import { modalActions } from '../store/modal-slice'
+import { informationActions } from '../store/information-slice'
 // SCSS
 import styles from './AccountClosingPage.module.scss'
 
@@ -15,6 +18,20 @@ const AccountClosingPage = () => {
   const pathname = useLocation().pathname
   const dispatch = useDispatch()
   const navigate = useNavigate()
+  // useSelector
+  const isAccountClosingModalOpen = useSelector(
+    (state) => state.modal.isAccountClosingModalOpen
+  )
+  // useState
+  const [twoThousand, setTwoThousand] = useState(0)
+  const [oneThousand, setOneThousand] = useState(0)
+  const [fiveHundred, setFiveHundred] = useState(0)
+  const [twoHundred, setTwoHundred] = useState(0)
+  const [oneHundred, setOneHundred] = useState(0)
+  const [fifty, setFifty] = useState(0)
+  const [ten, setTen] = useState(0)
+  const [five, setFive] = useState(0)
+  const [one, setOne] = useState(0)
 
   // 確認登入狀態
   useEffect(() => {
@@ -24,139 +41,201 @@ const AccountClosingPage = () => {
     }
   }, [navigate])
 
+  // 取得未結帳營收
+  useEffect(() => {
+    const getUnsettledRevenue = async () => {
+      const res = await getUnsettledRevenueApi()
+      dispatch(informationActions.setUnSettledRevenue(res.data))
+    }
+    getUnsettledRevenue()
+  }, [dispatch])
+
+  // 計算金額加總
+  const startCalculateHandler = () => {
+    dispatch(
+      informationActions.setAccountClosingCalculate(
+        2000 * twoThousand +
+          1000 * oneThousand +
+          500 * fiveHundred +
+          200 * twoHundred +
+          100 * oneHundred +
+          50 * fifty +
+          10 * ten +
+          5 * five +
+          1 * one
+      )
+    )
+    dispatch(modalActions.setIsAccountClosingModalOpen(true))
+  }
+
   return (
     <div className='main__container'>
-      <AccountClosingModal />
+      <AccountClosingModal trigger={isAccountClosingModalOpen} />
       <PosMainGridSystem pathname={pathname}>
         <div className={styles.container__height}>
           <FormSwitchButton page='account' />
           <div className={styles.content__container}>
             <form className={styles.calculate__wrapper}>
               <table>
-                <tr>
-                  <th className={styles.left__top}>金額</th>
-                  <th className={styles.right__top}>數量</th>
-                </tr>
-                <tr>
-                  <td>
-                    <label className={styles.label} htmlFor='two__thousand'>
-                      $2000
-                    </label>
-                  </td>
-                  <td>
-                    <input
-                      className={styles.input}
-                      type='text'
-                      id='two__thousand'
-                    />
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <label className={styles.label} htmlFor='one__thousand'>
-                      $1000
-                    </label>
-                  </td>
-                  <td>
-                    <input
-                      className={styles.input}
-                      type='text'
-                      id='one__thousand'
-                    />
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <label className={styles.label} htmlFor='five__hundred'>
-                      $500
-                    </label>
-                  </td>
-                  <td>
-                    <input
-                      className={styles.input}
-                      type='text'
-                      id='five__hundred'
-                    />
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <label className={styles.label} htmlFor='two__hundred'>
-                      $200
-                    </label>
-                  </td>
-                  <td>
-                    {' '}
-                    <input
-                      className={styles.input}
-                      type='text'
-                      id='two__hundred'
-                    />
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <label className={styles.label} htmlFor='one__hundred'>
-                      $100
-                    </label>
-                  </td>
-                  <td>
-                    <input
-                      className={styles.input}
-                      type='text'
-                      id='one__hundred'
-                    />
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <label className={styles.label} htmlFor='fifty'>
-                      $50
-                    </label>
-                  </td>
-                  <td>
-                    <input className={styles.input} type='text' id='fifty' />
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <label className={styles.label} htmlFor='ten'>
-                      $10
-                    </label>
-                  </td>
-                  <td>
-                    <input className={styles.input} type='text' id='ten' />
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <label className={styles.label} htmlFor='five'>
-                      $5
-                    </label>
-                  </td>
-                  <td>
-                    <input className={styles.input} type='text' id='five' />
-                  </td>
-                </tr>
-                <tr>
-                  <td className={styles.left__bottom}>
-                    <label className={styles.label} htmlFor='one'>
-                      $1
-                    </label>
-                  </td>
-                  <td className={styles.right__bottom}>
-                    <input className={styles.input} type='text' id='one' />
-                  </td>
-                </tr>
+                <thead>
+                  <tr>
+                    <th className={styles.left__top}>金額</th>
+                    <th className={styles.right__top}>數量</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>
+                      <label className={styles.label} htmlFor='two__thousand'>
+                        $2000
+                      </label>
+                    </td>
+                    <td>
+                      <input
+                        className={styles.input}
+                        type='number'
+                        id='two__thousand'
+                        onChange={(e) => setTwoThousand(e.target.value)}
+                        autoComplete='off'
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <label className={styles.label} htmlFor='one__thousand'>
+                        $1000
+                      </label>
+                    </td>
+                    <td>
+                      <input
+                        className={styles.input}
+                        type='number'
+                        id='one__thousand'
+                        onChange={(e) => setOneThousand(e.target.value)}
+                        autoComplete='off'
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <label className={styles.label} htmlFor='five__hundred'>
+                        $500
+                      </label>
+                    </td>
+                    <td>
+                      <input
+                        className={styles.input}
+                        type='number'
+                        id='five__hundred'
+                        onChange={(e) => setFiveHundred(e.target.value)}
+                        autoComplete='off'
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <label className={styles.label} htmlFor='two__hundred'>
+                        $200
+                      </label>
+                    </td>
+                    <td>
+                      <input
+                        className={styles.input}
+                        type='number'
+                        id='two__hundred'
+                        onChange={(e) => setTwoHundred(e.target.value)}
+                        autoComplete='off'
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <label className={styles.label} htmlFor='one__hundred'>
+                        $100
+                      </label>
+                    </td>
+                    <td>
+                      <input
+                        className={styles.input}
+                        type='number'
+                        id='one__hundred'
+                        onChange={(e) => setOneHundred(e.target.value)}
+                        autoComplete='off'
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <label className={styles.label} htmlFor='fifty'>
+                        $50
+                      </label>
+                    </td>
+                    <td>
+                      <input
+                        className={styles.input}
+                        type='number'
+                        id='fifty'
+                        onChange={(e) => setFifty(e.target.value)}
+                        autoComplete='off'
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <label className={styles.label} htmlFor='ten'>
+                        $10
+                      </label>
+                    </td>
+                    <td>
+                      <input
+                        className={styles.input}
+                        type='number'
+                        id='ten'
+                        onChange={(e) => setTen(e.target.value)}
+                        autoComplete='off'
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <label className={styles.label} htmlFor='five'>
+                        $5
+                      </label>
+                    </td>
+                    <td>
+                      <input
+                        className={styles.input}
+                        type='number'
+                        id='five'
+                        onChange={(e) => setFive(e.target.value)}
+                        autoComplete='off'
+                      />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className={styles.left__bottom}>
+                      <label className={styles.label} htmlFor='one'>
+                        $1
+                      </label>
+                    </td>
+                    <td className={styles.right__bottom}>
+                      <input
+                        className={styles.input}
+                        type='number'
+                        id='one'
+                        onChange={(e) => setOne(e.target.value)}
+                        autoComplete='off'
+                      />
+                    </td>
+                  </tr>
+                </tbody>
               </table>
             </form>
           </div>
           <div className={styles.button__container}>
             <button
               className={styles.submit__button}
-              onClick={() => {
-                dispatch(modalActions.setIsAccountClosingModalOpen(true))
-              }}
+              onClick={startCalculateHandler}
             >
               開始結算
             </button>
