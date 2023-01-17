bdee3f2163e55e7ad61c64c824a62bd3bbae505a
JamieLoLo <jamielo0320@gmail.com>
2023-01-17 18:29:04 +0800

    Add comment

--- src/POSComponents/OrderTableItem.jsx
+++ src/POSComponents/OrderTableItem.jsx
@@ -33,6 +33,7 @@ const OrderTableItem = ({ data }) => {
 
   // 點擊桌子後將桌子資訊存入store，判斷目前狀態，導入對應頁面。
   const orderHandler = async () => {
+    // 排除沒有的桌子
     if (data.name === '0') {
       return
     }
